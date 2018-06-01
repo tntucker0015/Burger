2dd0cc92d79e733455e0234fa1c87cbe16bb5a7e
Tim Tucker <tntucker0015@outlook.com>
2018-06-01 09:34:08 -0400

    adding  schema and seeds data for table population

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Fried Green Tomato Burger');
+INSERT INTO burgers (name) VALUES ('Mushroom Melt');
+INSERT INTO burgers (name, devoured) VALUES ('Bacon & Blue', true);
+INSERT INTO burgers (name, devoured) VALUES ('Frito Bandito', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Carolina', true);
+INSERT INTO burgers (name) VALUES ('The Texan');
+INSERT INTO burgers (name) VALUES ('The Philly');
+INSERT INTO burgers (name) VALUES ('The Greek');
